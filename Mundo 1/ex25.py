62dc8e79d1e13f115af96e14e0c609aedce013c5
wesleykleim <wesleykleim@gmail.com>
2022-09-12 20:53:59 -0300

    exercicios

--- Mundo 1/ex25.py
+++ Mundo 1/ex25.py
@@ -2,7 +2,7 @@ numero = (int(input('Digite um numero inteiro: ')))
 
 fatorial= 1
 contador = 1
-while contador <= numero:
+while contador  <=  numero:
     fatorial = fatorial * contador
     contador += 1
 print(fatorial)
---
+++ Mundo 1/ex26.py
@@ -0,0 +1,7 @@
+lista =[]
+cont = 0 
+idade = int(input('Digite quantas idades vai digitar? '))
+for cont in len(idade):
+    idade1 = int('Digite idade')
+    lista.append[cont]
+print(lista)
