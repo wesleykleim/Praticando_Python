d51d6d929133703d0fc091907353da5a68c31f2f
wesleykleim <wesleykleim@gmail.com>
2022-09-12 20:51:18 -0300

    fatorial

---
+++ Mundo 1/ex25.py
@@ -0,0 +1,10 @@
+numero = (int(input('Digite um numero inteiro: ')))
+
+fatorial= 1
+contador = 1
+while contador <= numero:
+    fatorial = fatorial * contador
+    contador += 1
+print(fatorial)
+    
+
