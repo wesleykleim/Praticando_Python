5bb5b4c9182bdfb9b72392858ab25b6e788708ac
wesleykleim <wesleykleim@gmail.com>
2022-09-12 22:29:18 -0300

    calcula hora funcionario

---
+++ Mundo 1/ex27.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual é o seu salario mensal '))
+horas = int(input('Quantas horas trabalha por mês '))
+valor_hora = salario / horas
+print('O valor da sua hora trabalhada é {:.2f}'.format(valor_hora))
